aa8ec0ac92d697220b15a614462702c554e4acc6
KayaKalkan <kayakalkan@pau.edu.tr>
2025-07-09 17:25:17 +0300

    Caching the inverse of a matrix
    This assignment consists of two functions named cacheSolve and makeCacheMatrix.
    The makeCacheMatrix Function
    This function creates a special "matrix" object whose inverse can be cached. Essentially, it stores a matrix and provides a place to store its inverse. It also includes functional methods such as setting the matrix, getting the matrix, setting the inverse of the matrix, and getting the inverse of the matrix.
    cacheSolve Function
    This function calculates the inverse of the special "matrix" object returned by makeCacheMatrix. If the inverse is already cached, it retrieves it directly from the cache; otherwise, it calculates it and stores it in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix
+## This assignment consists of two functions named "makeCacheMatrix" and "cacheSolve".
 
-## Write a short comment describing this function
+## The "makeCacheMatrix" Function creates a special "matrix" 
+## object whose inverse can be cached. 
+## Essentially, it stores a matrix and provides a place to store its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
+        matrix_inv <- NULL 
+        
+        set <- function(y) {
+                x <<- y
+                matrix_inv <<- NULL 
+        }
+        
+        get <- function() x
+        setInv <- function(inv) matrix_inv <<- inv
+        getInv <- function() matrix_inv
+        
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
+}
 
+## The "cacheSolve" Function calculates the inverse of the special "matrix" object
+## returned by makeCacheMatrix. 
+cacheSolve <- function(x, ...) {
+        m_inv <- x$getInv()
+        
+        if (!is.null(m_inv)) {
+                return(m_inv)
+        }
+        
+        data <- x$get()
+        m_inv <- solve(data, ...)
+        x$setInv(m_inv)
+        m_inv
 }
 
 
